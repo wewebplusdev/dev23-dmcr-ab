3d31803d0c17e26ad501f46480c7be94bcba05a3
OhmMiee <48233965+OhmMiee@users.noreply.github.com>
2023-07-10 16:32:21 +0700

    update: js path

--- _html/inc/loadscript.php
+++ _html/inc/loadscript.php
@@ -1,13 +1,13 @@
 <!-- Core -->
-<script src="assets/js/libs/jquery-2.2.4.js"></script>
-<script src="assets/js/libs/jquery.easing.min.js"></script>
-<script src="assets/js/libs/jquery.lazyload.min.js"></script>
-<script src="assets/js/libs/modernizr.min.js"></script>
-<script src="assets/js/libs/bootstrap.min.js"></script>
-<script src="assets/js/libs/popper.min.js"></script>
-<script src="assets/js/libs/sweetalert.min.js"></script>
-<script src="assets/js/libs/validator.min.js"></script>
+<script src="<?php echo $core_template; ?>assets/js/libs/jquery-2.2.4.js"></script>
+<script src="<?php echo $core_template; ?>assets/js/libs/jquery.easing.min.js"></script>
+<script src="<?php echo $core_template; ?>assets/js/libs/jquery.lazyload.min.js"></script>
+<script src="<?php echo $core_template; ?>assets/js/libs/modernizr.min.js"></script>
+<script src="<?php echo $core_template; ?>assets/js/libs/bootstrap.min.js"></script>
+<script src="<?php echo $core_template; ?>assets/js/libs/popper.min.js"></script>
+<script src="<?php echo $core_template; ?>assets/js/libs/sweetalert.min.js"></script>
+<script src="<?php echo $core_template; ?>assets/js/libs/validator.min.js"></script>
 
 <!-- Custom -->
-<script src="assets/js/main.js<?=$modify?>"></script>
+<script src="<?php echo $core_template; ?>assets/js/main.js<?=$modify?>"></script>
 <script type="text/javascript">var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;</script>
