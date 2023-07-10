57c338223e4111c6bb2b85b86030b406df25b7b4
OhmMiee <48233965+OhmMiee@users.noreply.github.com>
2023-07-10 16:16:41 +0700

    create: font, favicon

--- _html/inc/loadstyle.php
+++ _html/inc/loadstyle.php
@@ -1,5 +1,5 @@
 <!-- Core -->
-
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
 <!-- Custom -->
 <?php $modify="?v=".date('Ymd').time(); ?>
