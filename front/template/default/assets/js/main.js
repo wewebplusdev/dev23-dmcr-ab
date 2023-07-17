02616ebeaa5bb48d220a0a6faf07a98440f33ca7
OhmMiee <48233965+OhmMiee@users.noreply.github.com>
2023-07-18 01:18:44 +0700

    update: main.js

--- front/template/default/assets/js/main.js
+++ front/template/default/assets/js/main.js
@@ -7,4 +7,31 @@ $(document).ready(function () {
     $('.layout-header .search-input .btn-close').click(function () {
         $('.search-input').addClass('hide');
     });
+
+    const herobannerSwiper = new Swiper('.hero-banner-swiper', {
+        // Optional parameters
+        slidesPerView: 1,
+        // spaceBetween: 30,
+        // direction: 'vertical',
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            dynamicBullets: true,
+            clickable: true,
+        },
+
+        // Navigation arrows
+        // navigation: {
+        //     nextEl: '.swiper-button-next',
+        //     prevEl: '.swiper-button-prev',
+        // },
+
+        // And if we need scrollbar
+        // scrollbar: {
+        //     el: '.swiper-scrollbar',
+        // },
+    });
+
 });
