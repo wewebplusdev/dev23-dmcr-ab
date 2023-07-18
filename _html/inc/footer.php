02a74c988fad7c7fdbb47050f093fe9c9947b769
OhmMiee <48233965+OhmMiee@users.noreply.github.com>
2023-07-18 16:25:42 +0700

    Update footer.php

--- _html/inc/footer.php
+++ _html/inc/footer.php
@@ -1,1 +1,173 @@
-<footer class="layout-footer lazy" data-bg="<?php echo $core_template; ?>assets/img/background/bg-footer.png">Footer</footer>
+<footer class="layout-footer lazy" data-bg="<?php echo $core_template; ?>assets/img/background/bg-footer.png">
+    <div class="container-lg">
+        <div class="vstack gap-3">
+            <div class="no-gutters row">
+                <div class="col-lg-6 col-12">
+                    <div class="brand">
+                        <div class="hstack gap-3">
+                            <div class="brand-logo position-relative d-sm-block d-none">
+                                <img alt="DMCR logo" src="<?php echo $core_template; ?>assets/img/static/logo.png" class="d-inline-block align-top lazy">
+                            </div>
+                            <div class="brand-title">
+                                <div class="title">ฐานข้อมูลปะการังเทียมและทุ่นในทะเล</div>
+                                <div class="subtitle">กรมทรัพยากรทางทะเลและชายฝั่ง</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="contact">
+                        <div class="row">
+                            <div class="col">
+                                <div class="c-address">
+                                    <div class="align-items-start hstack gap-2">
+                                        <div class="icon">
+                                            <img alt="icon-map-pin.svg" src="<?php echo $core_template; ?>assets/img/icon/icon-map-pin.svg" class="lazy">
+                                        </div>
+                                        <p class="desc">เลขที่ 120 หมู่ 3 อาคาร รัฐประศาสนภักดี
+                                            ศูนย์ราชการเฉลิมพระเกียรติ 80 พรรษา 5 ธันวาคม 2550 ถนนแจ้งวัฒนะ
+                                            แขวงทุ่งสองห้อง เขตหลักสี่ กรุงเทพฯ 10210</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="justify-content-lg-between row">
+                            <div class="py-sm-0 py-2 col-sm-auto">
+                                <div class="c-phone">
+                                    <div class="hstack gap-2">
+                                        <div class="icon">
+                                            <img alt="icon-telephone.svg" src="<?php echo $core_template; ?>assets/img/icon/icon-telephone.svg" class="lazy">
+                                        </div>
+                                        <p>
+                                            โทรศัพท์
+                                            <a class="d-block" href="tel:+66 2141 1300">
+                                                <span>+66 2141 1300</span>
+                                            </a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="py-sm-0 py-2 col-sm-auto">
+                                <div class="c-fax">
+                                    <div class="hstack gap-2">
+                                        <div class="icon">
+                                            <img alt="icon-fax.svg" src="<?php echo $core_template; ?>assets/img/icon/icon-fax.svg" class="lazy">
+                                        </div>
+                                        <p>แฟกซ์<span class="d-block">+66 2143 9242</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="py-sm-0 py-2 col-sm-auto">
+                                <div class="c-mail">
+                                    <div class="hstack gap-2">
+                                        <div class="icon">
+                                            <img alt="icon-mail.svg" src="<?php echo $core_template; ?>assets/img/icon/icon-mail.svg" class="lazy">
+                                        </div>
+                                        <p>
+                                            อีเมล
+                                            <a class="d-block" href="mailto:it@dmcr.mail.go.th">
+                                                <span>it@dmcr.mail.go.th</span>
+                                            </a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-7">
+                    <div class="info">
+                        <p class="desc">
+                            <strong>สงวนลิขสิทธิ์ © พ.ศ. 2556 กรมทรัพยากรทางทะเลและชายฝั่ง<br></strong>
+                            <span>พัฒนาระบบโดย กองสารสนเทศและเทคโนโลยีการสำรวจทรัพยากรทางทะเลและชายฝั่ง
+                                <br>ห้ามนำส่วนหนึ่งส่วนใดในเว็บไซต์นี้ไปทำซ้ำหรือเผยแพร่ในรูปแบบใดๆ
+                                หรือวิธีอื่นใดยกเว้นเพื่อวัตถุประสงค์ทางการศึกษา
+                                <br>หากมีความประสงค์ใช้ข้อมูลตัวเลขหรือข้อมูลเชิงพื้นที่ในการอ้างอิงโปรดสอบทานความถูกต้องกับหน่วยงานโดยตรง
+                                <br>เริ่มใช้งานตั้งแต่ กรกฎาคม พ.ศ. 2556
+                            </span>
+                        </p>
+                    </div>
+                </div>
+                <div class="ms-lg-auto col-lg-5">
+                    <div class="justify-content-lg-end row row-cols-auto">
+                        <div class="mb-lg-4 mt-3 col-xl-8 col-12 order-lg-1 order-2">
+                            <div class="validate">
+                                <div class="justify-content-lg-end hstack gap-2">
+                                    <div class="ipv6">
+                                        <a href="http://ipv6-test.com/validate.php?" title="ipv6 ready'" target="_self" rel="nofollow">
+                                            <img alt="ipv6 ready" src="<?php echo $core_template; ?>assets/img/icon/button-ipv6-big.png" class="lazy" style="color:transparent" width="74" height="36">
+                                        </a>
+                                    </div>
+                                    <div class="w3c">
+                                        <a href="http://www.w3.org/WAI/WCAG2A-Conformance" title="Explanation of WCAG 2.0 Level A Conformance" target="_self" rel="nofollow">
+                                            <img alt="Level A conformance, W3C WAI Web Content Accessibility Guidelines 2.0" src="https://www.w3.org/WAI/wcag2A" class="lazy" style="color:transparent;user-select:none" width="97" height="36">
+                                        </a>
+                                    </div>
+                                    <div class="w3css">
+                                        <a href="https://jigsaw.w3.org/css-validator/check/referer" rel="nofollow">
+                                            <img alt="Valid CSS!" src="https://jigsaw.w3.org/css-validator/images/vcss" class="lazy" style="color:transparent;border:0" width="97" height="36">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mt-lg-0 mt-3 col-auto order-lg-2 order-1">
+                            <div class="wg-social">
+                                <div class="social-list">
+                                    <div class="hstack gap-2">
+                                        <a class="link rounded-circle fb" title="Facebook" target="_blank" style="background:#1877F2" href="https://www.facebook.com/DMCRTH">
+
+                                        </a>
+                                        <a class="link rounded-circle tw" title="Twitter" target="_blank" style="background:#1DA1F2" href="https://twitter.com/dmcrth">
+
+                                        </a>
+                                        <a class="link rounded-circle ig" title="Instagram" target="_blank" style="background:radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%)" href="https://www.instagram.com/dmcrth/">
+
+                                        </a>
+                                        <a class="link rounded-circle yt" title="Youtube" target="_blank" style="background:#FF0000" href="https://www.youtube.com/channel/UC4_fXOSelonvCzuv0jc4qbg">
+                                       
+                                        </a>
+                                        <a class="link rounded-circle line" title="Line" target="_blank" href="https://line.me/ti/p/~@DMCRTH">
+                                            <div class="icon">
+                                                <img alt="Line" src="<?php echo $core_template; ?>assets/img/icon/icon-line.png" class="lazy">
+                                            </div>
+                                        </a>
+                                        <a class="tt" title="Tiktok" target="_blank" href="https://www.tiktok.com/@dmcrth">
+                                            <div class="icon">
+                                                <img alt="Tiktok" src="<?php echo $core_template; ?>assets/img/icon/icon-tiktok.png" class="lazy">
+                                            </div>
+                                        </a>
+                                        <a class="link rounded-circle back-office" title="Back Office" target="_blank" href="/weadmin">
+                                            <div class="icon">
+                                                <img alt="Back Office" src="<?php echo $core_template; ?>assets/img/icon/icon-back-office.svg" class="lazy">
+                                            </div>
+                                        </a>
+                                        <a class="link rounded-circle one-account" title="One Account" target="_blank" href="https://oneaccount.dmcr.go.th/weadmin/">
+                                            <div class="icon">
+                                                <img alt="One Account" src="<?php echo $core_template; ?>assets/img/icon/social-login.png" class="lazy">
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="justify-content-md-between mt-3 row row-cols-auto">
+                <div class="col-md-auto col-12">
+                    <div class="policies">
+                        <div class="policy-list hstack gap-3">
+                            <button type="button" class="link btn btn-link"><span>นโยบายการคุ้มครองข้อมูลส่วนบุคคล</span></button><button type="button" class="link btn btn-link"><span>นโยบายเว็บไซต์</span></button><button type="button" class="link btn btn-link"><span>นโยบายการรักษาความมั่นคงปลอดภัยเว็บไซต์</span></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-auto col-12">
+                    <div class="text-center">Visits</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
